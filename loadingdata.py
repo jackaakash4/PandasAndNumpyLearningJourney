25afd52872cff397dbcdfd6cd3d4bc4484e7658a
Jack Aakash <jackaakash4@gmail.com>
2026-01-15 23:38:51 +0545

    Learned: Viewed, explored and handling missing data

--- loadingdata.py
+++ loadingdata.py
@@ -6,3 +6,15 @@ df = pd.read_csv("people_data.csv")
 print(df.head())
 
 
+#to get info
+print(df.info())
+
+
+#handling missing data
+print("Handling missing data \n")
+print(df.isnull().sum())
+
+
+#filling the emply with )
+df = df.fillna(0)
+
