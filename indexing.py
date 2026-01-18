414a3dfdbcfcf538d4816c162f48ac85e1b49ed2
Jack Aakash <jackaakash4@gmail.com>
2026-01-18 14:17:29 +0545

    Learned: to select the multiple columns from dataset

--- indexing.py
+++ indexing.py
@@ -11,3 +11,9 @@ print(data.head(5))
 first = data["First Name"]
 print("\nSingle column selected from dataset")
 print(first.head(5))
+
+#selecting multiple columns
+
+columns = data[["First Name", "Last Name"]]
+print("\nSelected multiple columns from dataset")
+print(columns.head(5))
