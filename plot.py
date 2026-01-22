c3305b7826fadfec55fa35fb433f3ad3b58647ef
Jack Aakash <jackaakash4@gmail.com>
2026-01-22 23:04:11 +0545

    Ploting the cos curve

---
+++ plot.py
@@ -0,0 +1,14 @@
+#QuickStart
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+X = np.linspace(0, 2 * np.pi, 100)
+Y = np.cos(X)
+
+fig, ax = plt.subplots()
+
+ax.plot(X, Y, color='green')
+
+plt.show()
