d1d6ed506aada2a7baedd857d533c394f50a5e94
Jack Aakash <jackaakash4@gmail.com>
2026-03-17 00:08:07 +0545

    Checked and filtered non missing value using notnull() method

--- missingData1.py
+++ missingData1.py
@@ -9,3 +9,19 @@ data = {'Name': ['Amit', 'Sita', np.nan, 'Raj'],
 df = pd.DataFrame(data)
 
 print(df.isna())
+
+#checking for non-missing values using notnull()
+#not null returns a df with boolean values where True indicate non-missing value
+
+non_missing_value = df.notnull()
+print("\nNon missing value")
+print(non_missing_value)
+
+#filtering data with non-missing value
+
+csv_data = pd.read_csv("employees.csv")
+non_missing_gender = pd.notnull(csv_data["Gender"])
+
+non_missing_gender_data = csv_data[non_missing_gender]
+print("\n Non missing gender data")
+print(non_missing_gender_data)
