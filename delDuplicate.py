4dfa7d5bbd2f91163acb0c998065b15daf884aa7
Jack Aakash <jackaakash4@gmail.com>
2026-03-19 23:04:59 +0545

    Learned: drop_duplicates() function to remove the duplicates

--- delDuplicate.py
+++ delDuplicate.py
@@ -10,12 +10,27 @@ data = {
 
 df = pd.DataFrame(data)
 
-print("Original DataFrame: ", df)
+print("Original DataFrame: \n", df)
 
 #after removing the duplicates row
 
 df_nodup = df.drop_duplicates()
 
-print("\nAfter removing duplicates from dataframe: ", df_nodup)
+print("\nAfter removing duplicates from dataframe:\n", df_nodup)
 
-        
+#dropping based on specific column
+df_cleaned = df.drop_duplicates(subset= ["City"])
+print("\nAfter removing duplicates from city column \n", df_cleaned)
+
+#keeping the last occurance of duplicates
+df_last = df.drop_duplicates(keep="last")
+print("\nKeepin=g the last occurance of duplicates \n", df_last)
+
+#dropping all duplicates
+df_all = df.drop_duplicates(keep = False)
+print("\nAfter dropping all duplicates \n", df_all)
+
+#modifying the original dataframe directily
+df.drop_duplicates(inplace = True)
+print("Modifying the original dataframe directly\n", df) 
+
