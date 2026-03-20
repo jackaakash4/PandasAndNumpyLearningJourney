1c47a449df5f9d0b4ac6149ad45a4a4269d3b361
Jack Aakash <jackaakash4@gmail.com>
2026-03-20 22:53:48 +0545

    Deleted all the columns with all NaN values

--- replaceEmpty.py
+++ replaceEmpty.py
@@ -12,3 +12,7 @@ print("Before changing empty string with null \n", df)
 
 df.replace("", np.nan, inplace = True)
 print("\nAfter changing\n",df)
+
+#deleting columns with all NaN values
+df.dropna(how = 'all', axis = 1, inplace = True)
+print("\nAfter deleting columns with all NaN values\n", df)
