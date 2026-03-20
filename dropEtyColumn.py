982ea64c264de899f70a8ffd3030794abd1a78fc
Jack Aakash <jackaakash4@gmail.com>
2026-03-20 22:44:32 +0545

    Dropped column with all empty values

--- dropEtyColumn.py
+++ dropEtyColumn.py
@@ -13,4 +13,11 @@ df = pd.DataFrame({
 df['Department'] = np.nan
 print(df)
 
+#remove all null values column
+df.dropna(how = 'all', axis = 1, inplace = True)
+#inplace = True means apply in the same dataframe rather than returning new dataframe 
+#how = 'all' means remove column where all values are nan
+#axis = 1 means column. And axis = 0 means index or row
+
+print("\nAfter removing columns with all empty values\n", df)
 
