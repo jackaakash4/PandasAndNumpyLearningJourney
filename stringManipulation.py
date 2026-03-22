8e9a0d56084756e34729fd3a1fecf2473b64abce
Jack Aakash <jackaakash4@gmail.com>
2026-03-22 21:36:49 +0545

    Encoded the column using get_dummies() and checked the starting of a string using startswith() method

--- stringManipulation.py
+++ stringManipulation.py
@@ -11,3 +11,11 @@ data = { 'Name': ['Lukas', 'Sofia', 'Hiroshi', 'Marta', 'Yannis', np.nan, 'Elena
 df = pd.DataFrame(data)
 
 print("Concat: \n", df['Name'].str.cat(sep=' '))
+
+#get_dummies() method converts each unique string into a separate one-hot encoded column for modeling
+print("Converted each unique string into a separate encoded column\n", df['City'].str.get_dummies())
+
+#startswith() to check whether each string begins with the specific prefix
+
+print("Checking: \n", df['Name'].str.startswith('M'))
+
