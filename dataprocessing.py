0af4c77a8a9e87ac9c90e9f1fa9a789e6504f0e4
Jack Aakash <jackaakash4@gmail.com>
2026-03-24 23:01:31 +0545

    Data Processing: Loading the data

---
+++ dataprocessing.py
@@ -0,0 +1,10 @@
+#data processing include data driven work which helps us to provide meaningful insights from the data
+
+#example
+
+import pandas as pd
+
+#loading data in pandas dataframe
+data_frame = pd.read_csv("employees.csv")
+
+
