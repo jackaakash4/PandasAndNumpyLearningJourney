8e6e64350defe058e46939b7df08bd0b957febe9
Jack Aakash <jackaakash4@gmail.com>
2026-03-24 23:10:50 +0545

    Dataprocessing: Summary and descriptive statistical measures of a dataframe

--- dataprocessing.py
+++ dataprocessing.py
@@ -8,6 +8,17 @@ import pandas as pd
 data_frame = pd.read_csv("employees.csv")
 
 #printing rows of the data
-print(data_frame.head())
+print("Dataframe's head: \n",data_frame.head())
 
-print(data_frame.tail())
+print("Dataframe's tail: \n",data_frame.tail())
+
+#printing the column names of the dataframe
+
+print("Columns name of the dataframe \n", list(data_frame.columns))
+
+#summary of the dataframe
+
+print("The summary of the dataframe is \n", data_frame.info())
+
+#descriptive statistical measures of a dataframe
+print("The descriptive statistical measures of a dataframe is \n", data_frame.describe())
