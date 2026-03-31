6fbcdb90d9cfaac1b9ef391b86be937c8bb409bd
Jack Aakash <jackaakash4@gmail.com>
2026-03-31 22:48:50 +0545

    Simple plot using matplotlib

---
+++ simplePlot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 1, 2, 3, 5, 8, 13]
+y = [2, 4, 6, 8, 10, 12, 14]
+
+plt.plot(x, y)
+
+plt.xlabel('Fibonacci')
+plt.ylabel('Y-axis')
+plt.title('Simple fibonacci plot')
+
+plt.show()
