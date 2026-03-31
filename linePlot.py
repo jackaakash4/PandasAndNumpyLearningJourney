914b004feae840c676ab92ceed594135e5b70d08
Jack Aakash <jackaakash4@gmail.com>
2026-03-31 22:57:16 +0545

    Plotted multiple line in single frame

--- linePlot.py
+++ linePlot.py
@@ -6,4 +6,16 @@ y1 = [1, 4, 9, 16, 25]
 y2 = [25, 20, 15, 10, 5]
 
 plt.plot(x, y1, label = 'y = x^2', color = 'green', linestyle = '--', marker = 'o')
+
+
+plt.plot(x, y2, label = 'y = 30 -  x^2', color = 'red', linestyle = '-', marker = 'x')
+
+
+plt.grid(True)
+
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title("customized line plot with multiple series")
+
+plt.legend()
 plt.show()
