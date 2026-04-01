ae2a83eebde3be13542e39d4efa78e3ec0da6ca0
Jack Aakash <jackaakash4@gmail.com>
2026-04-01 23:25:15 +0545

    Sub graph using matplotlib

---
+++ subplot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+a = [1, 2, 3, 4, 5]
+b = [0, 0.6, 0.2, 15, 10, 8, 16, 21]
+c = [4, 2, 6, 8, 3, 20, 13, 15]
+
+fig = plt.figure(figsize = (10, 10))
+
+sub1 = plt.subplot(2, 2, 1)
+sub2 = plt.subplot(2, 2, 2)
+sub3 = plt.subplot(2, 2, 3)
+sub4 = plt.subplot(2, 2, 4)
+
+sub1.plot(a, 'sb')
+
+plt.show()
