ed6b3b53c654c90ea156581f39cddf3da4234749
Jack Aakash <jackaakash4@gmail.com>
2026-04-02 23:20:19 +0545

    Added the label in the graph

--- simpleline.py
+++ simpleline.py
@@ -4,5 +4,10 @@ import numpy as np
 x = np.array([1, 2, 3, 4])
 y = x ** 2
 
+plt.title("Simple line plot with label")
+plt.xlabel("x")
+plt.ylabel("y = x^2")
 plt.plot(x, y, marker = 'x')
 plt.show()
+
+
