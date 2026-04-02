8f3606a7076ee55b5b9d41727065bf9bd477b806
Jack Aakash <jackaakash4@gmail.com>
2026-04-02 23:22:43 +0545

    Additional info like markers, linestyle and label and legend

--- simpleline.py
+++ simpleline.py
@@ -5,9 +5,11 @@ x = np.array([1, 2, 3, 4])
 y = x ** 2
 
 plt.title("Simple line plot with label")
+
 plt.xlabel("x")
 plt.ylabel("y = x^2")
-plt.plot(x, y, marker = 'x')
+plt.plot(x, y, marker = 'o', linestyle = '--', label = 'Datapoint')
+plt.legend()
 plt.show()
 
 
