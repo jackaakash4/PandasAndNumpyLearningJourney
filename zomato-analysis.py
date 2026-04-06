54cc199b18e5c8b0a47d12960acb5ec40bb9de14
Jack Aakash <jackaakash4@gmail.com>
2026-04-06 23:15:47 +0545

    Data Cleaning and preparation: changing rate column into float

--- zomato-analysis.py
+++ zomato-analysis.py
@@ -7,4 +7,17 @@ import seaborn as sns
 
 #creating the dataframe
 dataframe = pd.read_csv("zomato.csv")
-print(dataframe.head())
+print("Original Dataframe \n",dataframe.head())
+
+#cleaning and preparation
+#convert the rate column to a float by removing denominator characters
+
+def handleRate(value):
+    value = str(value).split('/')
+    value = value[0]
+    return float(value)
+
+dataframe['rate'] = dataframe['rate'].apply(handleRate)
+print("\nAfter change in rate's datatype",dataframe.head())
+
+print("\nDataframe's info: \n", dataframe.info())
