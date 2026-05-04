fc9c8a6f80e40acd85f3eebcd58b9560efcd8001
Jack Aakash <jackaakash4@gmail.com>
2026-05-04 22:22:13 +0545

    Summarize the dataset

--- iris/iris_analysis.py
+++ iris/iris_analysis.py
@@ -6,3 +6,12 @@ df = pd.read_csv("iris.csv")
 
 #printing the top rows
 print(df.head())
+
+#getting the information about the dataset
+print("Information about the dataset\n", df.shape)
+
+
+print(df.info())
+
+#summary of the dataset
+print("\nSummary of the dataset", df.describe())
