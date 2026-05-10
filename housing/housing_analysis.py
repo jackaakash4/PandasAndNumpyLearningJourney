8e928fbb51b198268c656a5d15b4cf22a13d58b1
Jack Aakash <jackaakash4@gmail.com>
2026-05-10 13:08:41 +0545

    Feat: Data preporecessed

--- housing/housing_analysis.py
+++ housing/housing_analysis.py
@@ -9,3 +9,16 @@ df = pd.read_excel("housing.xlsx")
 print("Dataset: \n", df.head())
 
 print("\nShape of dataset \n", df.shape)
+print("\nSummary of dataset \n", df.describe())
+
+#dataprocessing
+#categorizing the features based on their data types
+
+object_cols = df.select_dtypes(include = ['object']).columns
+print("Categorical variables: ", len(object_cols), "\t", object_cols)
+
+int_cols = df.select_dtypes(include = ['int64']).columns
+print("Integer variables: ", len(int_cols), "\t", int_cols)
+
+float_cols = df.select_dtypes(include = ['float64']).columns
+print("Float variables: ", len(float_cols), "\t", float_cols)
