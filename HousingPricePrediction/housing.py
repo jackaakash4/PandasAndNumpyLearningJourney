1500599a7b1f724139a5e3d17a25b35430ed9a04
jackaakash4 <jackaakash4@gmail.com>
2026-05-12 12:16:14 +0545

    Visualized the dataset ini histogramA

--- HousingPricePrediction/housing.py
+++ HousingPricePrediction/housing.py
@@ -14,3 +14,12 @@ print("Housing dataset: \n", dataset.head())
 print("\nSummary of dataset: \n", dataset.describe())
 print("\nShape of datset: \n", dataset.shape)
 print("\nInfo of dataset: \n", dataset.info())
+
+#since all the features are numeric except ocean_proximity is object. Since it has repetative value it must be categorical value so counting it.
+
+print("\n Count the ocean_proximity's value : \t", dataset['ocean_proximity'].value_counts())
+
+#visualizing the whole dataset
+
+dataset.hist(bins = 64, figsize = (12, 8))
+plt.show()
