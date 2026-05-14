9ba9ca645f754c7b2a6bf81dd8babdca7b268fa0
Jack Aakash <jackaakash4@gmail.com>
2026-05-15 00:08:14 +0545

    Random Forest Regression and its mean absolute percentage error
    it is ensemble learning algorithm used for both classification and
    regression tasks.
    it constructs multiple decision trees during training
    where each tree in the forest is built on a random subset of the data
    and features ensuring the diversity in the model
    The final output is determined by averaging the outputs of individual
    tree(for regression)

--- housing/housing_analysis.py
+++ housing/housing_analysis.py
@@ -149,6 +149,17 @@ mape = mean_absolute_percentage_error(Y_valid, Y_pred)
 
 print("\nMean absolute percentage error: \t", mape)
 
+#2. Random Forest Regression
+from sklearn.ensemble import RandomForestRegressor
+
+model_RFR = RandomForestRegressor(n_estimators = 10)
+model_RFR.fit(X_train, Y_train)
+Y_pred = model_RFR.predict(X_valid)
+
+mape = mean_absolute_percentage_error(Y_valid, Y_pred)
+print("\nMAPE using random forest regression \n", mape)
+
+
 
 
 
